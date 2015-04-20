f5ebcd6f9e43c5cc2a5eec3e0b9b52f5fd53d305
Radfordhound <Radfordhound@users.noreply.github.com>
2015-04-20 01:12:26 -0500

    Re-arranged project some more

---
+++ GMSharp/GMSharp(WP)/Resources/Room.cs
@@ -0,0 +1,20 @@
+﻿using GMSharp.Resources;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GMSharp.Resources
+{
+    public class Room : Resource
+    {
+        public List<Object> objects = new List<Object>();
+
+        public void Update()
+        {
+            foreach (Object obj in objects)
+            {
+                obj.Update();
+            }
+        }
+    }
+}
